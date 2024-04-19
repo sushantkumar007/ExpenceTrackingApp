713c2489d65a0b5fe4a4eec188ca10a04fec5944
sushantkumar51 <131727757+sushantkumar51@users.noreply.github.com>
2024-04-19 16:50:21 +0000

    add conf auth and config file

---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import { Client, Account, ID } from "appwrite"
+import conf from '../conf/conf'
+
+class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const user = await this.account.create(ID.unique, email, password, name)
+            if (user) {
+                return this.login({email, password})
+            } else {
+                return user
+            }
+        } catch (error) {
+            console.log("appwrite :: authService :: createAccount ", error)
+            return false
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            console.log("appwrite :: authService :: login ", error)
+            return false
+        }
+    }
+
+    async logout() {
+        try {
+            await this.account.deleteSessions()
+            return true;
+        } catch (error) {
+            console.log("appwrite :: authService :: logout ", error)
+            return false
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return this.account.get()
+        } catch (error) {
+            console.log("appwrite :: authService :: getCurrentUser ", error)
+        }
+    }
+
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ src/appwrite/config.js
@@ -0,0 +1,100 @@
+import { Client, Databases, ID, Query } from 'appwrite'
+import conf from '../conf/conf'
+
+class DatabaseService {
+    client = new Client();
+    databases;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client)
+    }
+
+    async createTransection({date, particular, amount, status, userId}) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                ID.unique(),
+                {
+                    date,
+                    particular,
+                    amount,
+                    status,
+                    userId  
+                }
+            )            
+        } catch (error) {
+            console.log("appwrite :: databaseService :: createDocument ", error)
+            return false
+        }
+    }
+
+    async updateTransection({transectionId, date, particular, amount, status, userId}) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                transectionId,
+                {
+                    date,
+                    particular,
+                    amount,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("appwrite :: databaseService :: updateTransection ", error)
+            return false;
+        }
+    }
+
+    async deleteTransection(transectionId) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                transectionId
+            )
+            return true
+        } catch (error) {
+            console.log("appwrite :: databaseService :: deleteTransection ", error)
+            return false
+        }
+    }
+
+    async getTransection(transectionId) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                transectionId
+            )
+        } catch (error) {
+            console.log("appwrite :: databaseService :: getTransection ", error)
+            return false;
+        }
+    }
+
+    async getStatement(userId) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                [
+                    Query.equal("userId", [userId])
+                ]
+            )
+        } catch (error) {
+            console.log("appwrite :: databaseService :: getStatement ", error)
+        }
+    }
+
+}
+
+const databaseService = new DatabaseService()
+
+export default databaseService;
